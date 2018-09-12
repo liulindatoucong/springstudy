7fc19594e9bcb6020cca1d51ea4b28cc4213cc6e
liulin <liulindatoucong@163.com>
2018-09-12 17:03:28 +0800

    完成自定义的一个spring功能框架

---
+++ src/main/java/config/WebAppInitailizer.java
@@ -0,0 +1,31 @@
+package config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitailizer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	/**
+	 * 指定业务配置类
+	 */
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class<?>[] {RootConfig.class};
+	}
+
+	/**
+	 * 指定controller、视图解析器以及处理器映射
+	 */
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class<?>[] {WebConfig.class};
+	}
+
+	/**
+	 * 将DispatcherServlet 映射到"/"
+	 */
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+
+}
---
+++ src/main/java/config/WebConfig.java
@@ -0,0 +1,54 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("web.controller")
+public class WebConfig extends WebMvcConfigurationSupport {
+
+	/**
+	 * 配置静态资源的处理
+	 */
+	@Override
+	protected void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+	/**
+	 * 配置thymeleaf识图解析器
+	 */
+	@Bean
+	public ViewResolver mvcViewResolver(SpringTemplateEngine templateEngine) {
+		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+		resolver.setTemplateEngine(templateEngine);
+		return resolver;
+	}
+	
+	@Bean
+	public SpringTemplateEngine templateEngine(SpringResourceTemplateResolver templateResolver)
+	{
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver);
+		return templateEngine;
+	}
+	
+	@Bean
+	public SpringResourceTemplateResolver templateResolver() {
+		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+		templateResolver.setPrefix("/WEB-INF/templates/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode("HTML5");
+		return templateResolver;	
+	}
+	
+}
